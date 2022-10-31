b5c4a1770df1691ff5eb31f2bc749db9a214900d
Tomasz Mikulski <tl.mikulski@gmail.com>
2022-10-31 23:56:41 +0100

    Docker compose configuration

---
+++ doBuildProject.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+docker compose up -d
+
+#project name is defined in docker-compose.yml
+docker exec -w //opt/code readtweets-php-1 composer install
+docker exec -w //opt/code readtweets-php-1 php bin/console app:get-twitter-posts
