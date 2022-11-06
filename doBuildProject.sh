b767f222611a1d6a930c7940cb878207a77484a4
Tomasz Mikulski <tl.mikulski@gmail.com>
2022-11-06 13:52:38 +0100

    Use new compose format, update for using with WSL2

--- doBuildProject.sh
+++ doBuildProject.sh
@@ -3,5 +3,5 @@
 docker compose up -d
 
 #project name is defined in docker-compose.yml
-docker exec -w //opt/code readtweets-php-1 composer install
-docker exec -w //opt/code readtweets-php-1 php bin/console app:get-twitter-posts
+docker exec -w /opt/code readtweets-php-1 composer install
+docker exec -w /opt/code readtweets-php-1 php bin/console app:get-twitter-posts
